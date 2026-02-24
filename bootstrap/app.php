60a60c9474de0ff19575d6abeb630a564bf6c2a2
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-02-24 12:28:07 +0530

    Add approved middleware alias for account approval check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'checkrole' => \App\Http\Middleware\CheckRole::class,
+            'approved' => \App\Http\Middleware\CheckApproved::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
