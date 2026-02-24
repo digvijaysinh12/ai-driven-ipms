58dabc65e3bbfdce696b13eb2497278b26cbd88e
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-02-24 12:25:38 +0530

    Add middleware to restrict access for unapproved users

---
+++ app/Http/Middleware/CheckApproved.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckApproved
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if($user && $user->status !== 'approved'){
+            Auth::logout();
+
+            return redirect()->route('login')
+                ->with('error','Your account is pending HR approval.');
+        }
+        return $next($request);
+    }
+}
