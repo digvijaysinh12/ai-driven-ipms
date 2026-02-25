f41b62536916189c162cd99afc0da53efcc527ad
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-02-25 14:50:09 +0530

    feat(mentor): add assignments module view
    - Created mentor assignments page
    - Integrated reusable card component
    - Prepared UI structure for managing intern assignments

---
+++ resources/views/mentor/assignments.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.mentor')
+
+@section('content')
+
+<x-card title="Assignments Module">
+
+    <p class="text-gray-500">
+        Create and manage intern assignments here.
+    </p>
+
+</x-card>
+
+@endsection
