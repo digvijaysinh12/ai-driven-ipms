1e3f7beaf8df6262af2c97d922485c14eb4ab1d5
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-02-25 14:51:01 +0530

    feat(mentor-interns): implement my interns module page

---
+++ resources/views/mentor/interns.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.mentor')
+
+@section('content')
+
+<x-card title="My Interns">
+
+    <p class="text-gray-500">
+        List of interns assigned to you will appear here.
+    </p>
+
+</x-card>
+
+@endsection
