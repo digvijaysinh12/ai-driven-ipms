f8f9d63a8cb78778bebda7eb59006cab742593ad
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-29 19:18:31 +0530

    Add migration 03 07 061027 a dd c od e t o q ue st io ns t ab le

---
+++ database/migrations/2026_03_07_061027_add_code_to_questions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+public function up()
+{
+Schema::table('questions', function (Blueprint $table) {
+
+$table->text('code')->nullable()->after('problem_statement');
+
+});
+}
+
+public function down()
+{
+Schema::table('questions', function (Blueprint $table) {
+
+$table->dropColumn('code');
+
+});
+}
+
+};
