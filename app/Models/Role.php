8ae304807eb60c371141c973f10c18e22914cf80
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-29 19:18:15 +0530

    Update r ol e model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
     public function users(){
         return $this->hasMany(User::class);
     }
