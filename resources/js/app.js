1c1c69659e2bae195f4bbeda9144d6afb32f81ee
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-29 19:13:55 +0530

    Remove stale exercise import from app entrypoint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+// Alpine.js for modal/dropdown if needed
+// import Alpine from 'alpinejs';
+// window.Alpine = Alpine;
+// Alpine.start();
